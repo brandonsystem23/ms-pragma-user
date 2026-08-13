4fdad19006bd981bee0c17a0e3b3744fa0c394cf
Brandon Briones Rojas <brandon20kaido@gmail.com>
2026-08-12 22:15:46 -0500

    Crear propietario

--- src/main/java/com/plazoleta/users_service/application/dto/request/CreateEmployeeRequest.java
+++ src/main/java/com/plazoleta/users_service/application/dto/request/CreateEmployeeRequest.java
@@ -1,41 +1,18 @@
 package com.plazoleta.users_service.application.dto.request;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 
 public record CreateEmployeeRequest(
 
-        @NotBlank(message = "El firstName es obligatorio")
         String firstName,
 
-        @NotBlank(message = "El lastName es obligatorio")
         String lastName,
 
-        @NotBlank(message = "El numberDocument de identidad es obligatorio")
-        @Pattern(
-                regexp = "\\d+",
-                message = "El numberDocument de identidad debe contener únicamente números"
-        )
         String numberDocument,
 
-        @NotBlank(message = "El phone es obligatorio")
-        @Size(
-                max = 13,
-                message = "El phone no puede tener más de 13 caracteres"
-        )
-        @Pattern(
-                regexp = "^\\+?\\d+$",
-                message = "El phone solo puede contener números y opcionalmente iniciar con +"
-        )
         String phone,
 
-        @NotBlank(message = "El email es obligatorio")
-        @Email(message = "El email no tiene un formato válido")
         String email,
 
-        @NotBlank(message = "La contraseña es obligatoria")
         String password
 ) {
 }
--- src/main/java/com/plazoleta/users_service/domain/exception/DomainErrorMessages.java
+++ src/main/java/com/plazoleta/users_service/domain/exception/DomainErrorMessages.java
@@ -2,20 +2,20 @@ package com.plazoleta.users_service.domain.exception;
 
 public final class DomainErrorMessages {
 
-    public static final String FIRST_NAME_REQUIRED = "El firstName es obligatorio";
-    public static final String LAST_NAME_REQUIRED = "El lastName es obligatorio";
-    public static final String DOCUMENT_REQUIRED = "El numberDocument de identidad es obligatorio";
-    public static final String DOCUMENT_NUMERIC = "El numberDocument de identidad debe contener únicamente números";
-    public static final String PHONE_REQUIRED = "El phone es obligatorio";
+    public static final String FIRST_NAME_REQUIRED = "El campo firstName es obligatorio";
+    public static final String LAST_NAME_REQUIRED = "El campo lastName es obligatorio";
+    public static final String DOCUMENT_REQUIRED = "El campo numberDocument es obligatorio";
+    public static final String DOCUMENT_NUMERIC = "El campo numberDocument debe contener únicamente números";
+    public static final String PHONE_REQUIRED = "El campo phone es obligatorio";
     public static final String PHONE_MAX_LENGTH = "El phone no puede tener más de 13 caracteres";
     public static final String PHONE_INVALID = "El phone solo puede contener números y opcionalmente iniciar con +";
-    public static final String BIRTH_DATE_REQUIRED = "La fecha de nacimiento es obligatoria";
+    public static final String BIRTH_DATE_REQUIRED = "El campo birthDate es obligatorio";
     public static final String USER_MUST_BE_ADULT = "El usuario debe tener 18 años o más";
-    public static final String EMAIL_REQUIRED = "El email es obligatorio";
+    public static final String EMAIL_REQUIRED = "El campo email es obligatorio";
     public static final String EMAIL_INVALID = "El email no tiene un formato válido";
     public static final String PASS_REQUIRED = "La contraseña es obligatoria";
 
-    public static final String DUPLICATE_DOCUMENT = "El numberDocument de identidad ya está registrado";
+    public static final String DUPLICATE_DOCUMENT = "El numero de documento  ya está registrado";
     public static final String DUPLICATE_EMAIL = "El email ya está registrado";
     public static final String INVALID_CREDENTIALS = "Credenciales inválidas";
     public static final String USER_NOT_FOUND = "Usuario no encontrado";
