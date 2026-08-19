cb577a6630a1c0a60491538d068e11378785503f
Brandon Briones Rojas <brandon20kaido@gmail.com>
2026-08-19 17:38:22 -0500

    Crear empleado

--- src/test/java/com/plazoleta/users_service/domain/service/RegisterUserServiceTest.java
+++ src/test/java/com/plazoleta/users_service/domain/service/RegisterUserServiceTest.java
@@ -22,7 +22,6 @@ import java.time.Month;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
